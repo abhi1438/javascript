17a1df5d1c61d021f7405ae9842a008ebf4178f5
abhi1438 <130151655+abhi1438@users.noreply.github.com>
2024-05-26 13:41:34 +0000

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId  = "A0001";
+let accountName = "Abhishek";
+var accountEmail = "abhishekdasari31@gmail.com";
+accountLocation = "Mumbai";
+let accountPassword;
+
+// accountId = "CN001";    not change because of using const 
+
+accountName = "Vikas";
+accountEmail = "vikas@gmail.com";
+accountLocation = "Solapur";
+
+
+console.table([accountId, accountName, accountEmail, accountLocation]);
